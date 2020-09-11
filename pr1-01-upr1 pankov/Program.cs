e24c1e70091b35b8d0927e20e1ff85c9321cf869
Evgesha8286 <pankov_er@rambler.ru>
2020-09-11 17:23:13 +0300

    1

---
+++ pr1-01-upr1 pankov/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace pr1_01_upr1_pankov
+{
+  class Program
+  {
+    struct Person
+    {
+      public string firstName;
+      public string LastName;
+      public int age;
+
+      public Person(string _firstName, string _lastName, int _age)
+      {
+        firstName = _firstName;
+        LastName = _lastName; 
+        age = _age;
+      }
+      public override string ToString()
+      {
+        return firstName + " " + LastName + ", age " + age;
+      }
+    }
+    static void Main(string[] args)
+    {
+      Person p = new Person("Tony", "Allen", 32); Console.WriteLine(p);
+    }
+  }
+}
